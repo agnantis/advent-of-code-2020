1f63e9a2edc668345939745efbdccd0f23e76b61
Kostas Agnantis <kagnadis@gmail.com>
2020-12-02 22:26:42 +0000

    Day 1 - Riddle 2

--- src/AoC/Day1.hs
+++ src/AoC/Day1.hs
@@ -1,6 +1,8 @@
 module AoC.Day1 where
 
 import Control.Arrow ((&&&))
+import Data.Maybe (fromMaybe)
+
 
 type Input = [Int]
 type Output = Int
@@ -8,21 +10,32 @@ type Output = Int
 total :: Int
 total = 2020
 
+error' :: a
+error' = error "Unable to find an answer"
+
 fstStar :: Input -> Output
-fstStar = go
- where
-  go [] = error "Unable to find a pair"
-  go (x:xs) = case (safeHead $ filter (\y -> x + y == total) xs) of
-                Just y -> x * y
-                Nothing -> go xs
+fstStar = fromMaybe error'
+        . fmap (uncurry (*))
+        . helper total
 
 sndStar :: Input -> Output
-sndStar = undefined
+sndStar = go
+ where
+   go (x:xs) = case (helper (total - x) xs) of
+                 Just (a, b) -> x * a * b
+                 Nothing -> go xs
+   go _ = error'
+
+helper :: Int -> Input -> Maybe (Int, Int)
+helper _ [] = Nothing
+helper _ [_] = Nothing
+helper i (x:xs) = case (safeHead . filter (\y -> x + y == i) $ xs) of
+              Just y -> Just $ (x, y)
+              Nothing -> helper i xs
 
 safeHead :: [a] -> Maybe a
 safeHead [] = Nothing
 safeHead (x:_) = Just x
-
 
 main :: IO  ()
 main = do
