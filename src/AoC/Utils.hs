06dfe80c84dd382c9af464d37a8ae7c321d41204
Kostas Agnantis <kagnadis@gmail.com>
2020-12-03 22:42:17 +0000

    Day 2 - Riddle 1

--- src/AoC/Utils.hs
+++ src/AoC/Utils.hs
@@ -1,18 +1,9 @@
 module AoC.Utils where
 
-import Data.Map as M
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
 
-orderPair :: Ord a => (a, a) -> (a, a)
-orderPair (a, b)
-  | a < b = (b, a)
-  | otherwise = (a, b)
-
-pairsCount :: Ord a => [(a,a)] -> [((a,a), Int)]
-pairsCount = M.toList . go M.empty
- where
-   go mp [] = mp
-   go mp (x:xs) = go newM xs
-    where
-      newM = M.insertWith (+) (orderPair x) 1 mp
-
+error' :: a
+error' = error "Unable to find an answer"
 
