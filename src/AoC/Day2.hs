c6470fb58db68652ac0619d795442fca64b7188a
Kostas Agnantis <kagnadis@gmail.com>
2020-12-04 20:56:50 +0000

    Day 2 - Riddle 2

--- src/AoC/Day2.hs
+++ src/AoC/Day2.hs
@@ -21,6 +21,22 @@ In the above example, 2 passwords are valid. The middle password, cdefg, is not;
 
 How many passwords are valid according to their policies?
 
+--- Part Two ---
+
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+    1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+    1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+    2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+How many passwords are valid according to the new interpretation of the policies?
+
 -}
 module AoC.Day2 where
 
@@ -30,28 +46,34 @@ data Entry = Entry
   { mn :: Int,
     mx :: Int,
     chr :: Char,
-    psw :: String
-  } deriving (Show)
+    pwd :: String
+  }
+  deriving (Show)
 
 type Input = [Entry]
 
 type Output = Int
 
 fstStar :: Input -> Output
-fstStar = length . filter validPass
-
-validPass :: Entry -> Bool
-validPass (Entry mn' mx' chr' pws') = 
-  let l = length $ filter ( == chr') pws'
-   in l >= mn' && l <= mx'
+fstStar = length . filter validPassA
 
 sndStar :: Input -> Output
-sndStar = undefined
+sndStar = length . filter validPassB
 
-parse:: String -> Entry
+validPassA :: Entry -> Bool
+validPassA (Entry mn' mx' chr' pwd') =
+  let l = length . filter (== chr') $ pwd'
+   in l >= mn' && l <= mx'
+
+validPassB :: Entry -> Bool
+validPassB (Entry mn' mx' chr' pwd') =
+  let l = length . filter (== chr') . fmap (pwd' !!) $ [mn' - 1, mx' - 1]
+   in l == 1
+
+parse :: String -> Entry
 parse l =
   let [a, (b : _), c] = words l
-      (mnS, '-':mxS) = break (== '-') a
+      (mnS, '-' : mxS) = break (== '-') a
    in Entry (read mnS) (read mxS) b c
 
 main :: IO ()
