777b4f9f447f82a23c5ba6aea165811ce85cdbbb
Kostas Agnantis <kagnadis@gmail.com>
2020-12-06 18:45:33 +0000

    Day 6 - Riddle 1

--- src/AoC/Utils.hs
+++ src/AoC/Utils.hs
@@ -8,6 +8,17 @@ fromEither :: a -> Either b a -> a
 fromEither a (Left _) = a
 fromEither _ (Right x) = x
 
+-- | 
+-- >>> splitOn ',' "a,bc,d,e"
+-- ["a", "bc", "d", "e"]
+--
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn v xs = 
+  let (ys, zs) = break (== v) xs
+      zs' = if null zs then zs else tail zs
+   in ys:(splitOn v zs')
+
 error' :: a
 error' = error "Unable to find an answer"
 
